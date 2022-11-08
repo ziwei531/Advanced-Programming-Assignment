4a0b0cd6dd17c63c708665ae5538dc49071ade01
Whoong Zi Wei <ziwei531@gmail.com>
2022-11-08 10:40:50 +0800

    added readme add by

---
+++ src/projectpartbprogram_group9/MainTestProgram.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectpartbprogram_group9;
+
+/**
+ *
+ * @author ziwei
+ */
+public class MainTestProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        /*
+         *  To execute your code that is outside of this MainTestProgram.java code, do YourClassName.methodName(); 
+            For example a user login scenario, we have a UserLogin.java
+            Hence, we do UserLogin.login() <- this is assuming you named the method login(). 
+            RMB TO PUT STATIC BEFORE YOUR METHOD ELSE IT WON'T WORK, like this -> public static void login() {}
+         */
+      
+
+        System.out.println("testing");
+    }
+    
+}
